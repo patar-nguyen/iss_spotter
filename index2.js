f4c1e1ccf81a4224d344b0967f9a693c8ef31f6e
Patrick Nguyen <patar.nguyen@gmail.com>
2022-01-20 20:43:42 -0500

    ISS Spotter

---
+++ index2.js
@@ -0,0 +1,18 @@
+// const { fetchMyIP } = require('./iss_promised');
+// const { fetchCoordsByIP } = require('./iss_promised');
+// const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+const { printPassTimes } = require('./index');
+
+// fetchMyIP()
+//   .then(fetchISSFlyOverTimes )
+//   .then(fetchCoordsByIP)
+//   .then(body => console.log(body));
+
+  nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
